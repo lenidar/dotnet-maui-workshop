0c9c5a5283d2c7bbec526f282cd950a7b62fa592
lenidar <55443074+lenidar@users.noreply.github.com>
2023-08-11 13:28:26 +0800

    Finished a module. Data Binding Module?

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,72 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel
+//[INotifyPropertyChanged]
+// partial class because the MVVM toolkit is generating the other half of the code
+
+// by inheriting ObservableObject will give more function in addition to INotifyPropertyChanged
+public partial class BaseViewModel : ObservableObject
 {
+    //private bool isBusy = false; // activity indicator
+    //private string title = ""; // title of the app
+
+    #region Will be removed because of the MVVM toolkit
+    //public bool IsBusy
+    //{
+    //    get => isBusy; // if getting, just returns the value
+    //    set // if setting (Changing the value)
+    //    {
+    //        if (isBusy == value) // checks if the value is the same
+    //            return; // if so, just returns actual value
+
+    //        isBusy = value; // if not the same, update the value
+    //        //OnPropertyChanged("IsBusy"); // notifies the view that a value has changed
+    //        // alternative
+    //        //OnPropertyChanged(nameof(IsBusy)); // nameof returns the string name of the method
+    //        OnPropertyChanged(); // this only works if CallerMemberName is used.
+    //        OnPropertyChanged(nameof(IsNotBusy)); // notifies MAUI that IsNotBusy is also updated
+    //    }
+    //}
+
+    //public string Title
+    //{
+    //    get => title;
+    //    set
+    //    {
+    //        if (title == value)
+    //            return;
+    //        title = value;
+    //        OnPropertyChanged();
+    //    }
+    //}
+
+    //public bool IsNotBusy => !IsBusy; // just updates the value of IsNotBusy to have the opposite value of IsBusy
+
+    //// event that maui is registering and signing up for
+    //public event PropertyChangedEventHandler PropertyChanged;
+
+    //// caller member name is a compile time event. 
+    //// if the OnPropertyChanged event is called and no string name has been called,
+    //// it will just assume its the name of the method that has been passed.
+    //public void OnPropertyChanged([CallerMemberName] string name = null)
+    ////public void OnPropertyChanged(string name)
+    //{
+    //    // is anyone subscribed to this event? If so, invoke it
+    //    // its going to pass itself and give it a new event which will pass the name property
+    //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    //}
+    #endregion
+
+    public BaseViewModel()
+    {
+        
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(isNotBusy))] //>>> NotifyPropertyChangedFor is replacing AlsoNotifyChangeFor >>> https://stackoverflow.com/questions/73295604/in-which-package-can-i-find-alsonotifychangefor-in-net-maui
+    bool isBusy = false;
+
+    [ObservableProperty]
+    string title = "";
+
+    public bool isNotBusy => !IsBusy;
 }
--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,46 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    MonkeyService monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+        
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+
+        
+    }
+
+    [RelayCommand] //>>> RelayCommand is replacing ICommand >>> https://github.com/CommunityToolkit/dotnet/releases/tag/v8.0.0-preview4
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if(monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach(var monkey in monkeys)
+                Monkeys.Add(monkey);
+        }
+        catch (Exception ex) 
+        { 
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!", 
+                $"Unable to get monkeys: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
